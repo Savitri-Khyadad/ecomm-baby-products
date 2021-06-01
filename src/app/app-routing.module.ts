392e8de1694f9f16267c72b3e3c52238da9e7ed0
SavitriKhyadad <savitrikhyadad15@gmail.com>
2021-06-01 20:01:44 +0530

    Added styling, sign up and login pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,16 +3,46 @@ import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
-import { SignupComponent } from './signup/signup.component';
-// import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './user/login/login.component';
+import { SignupComponent } from './user/signup/signup.component';
+import { WrapperComponent } from './Wrapper.component';
+import { ProductsComponent } from './products/products.component';
+import { CartComponent } from './cart/cart.component';
+import { WishlistComponent } from './wishlist/wishlist.component';
+import { UserComponent } from './user/user.component';
+import { User1Component } from './user/user1.component';
 
 const routes: Routes = [
-  {path:'about',component:AboutComponent},
-  {path:'contact',component:ContactComponent},
-  {path:'login',component:LoginComponent},
-  {path:'signup',component:SignupComponent},
-  {path:'home',component:HomeComponent}
+  // {path:'about',component:AboutComponent},
+  // {path:'contact',component:ContactComponent},
+  // {path:'login',component:LoginComponent},
+  // {path:'signup',component:SignupComponent},
+  // {path:'',component:HomeComponent},
+  {
+    path: 'about', component: WrapperComponent,
+    children: [{ path: '', component: AboutComponent }]
+  },
+  {
+    path: 'contact', component: WrapperComponent,
+    children: [{ path: '', component: ContactComponent }]
+  },
+  {
+    path: '', component: WrapperComponent,
+    children: [{ path: '', component: HomeComponent }]
+  },
+  { path: 'cart', component: CartComponent },
+  { path: 'wishlist', component: WishlistComponent },
+  {
+    path: 'signup', component: UserComponent,
+    children: [{ path: '', component: SignupComponent }]
+  },
+  {
+    path: 'login', component: User1Component,
+    children: [{ path: '', component: LoginComponent }]
+  },
+  // {
+  //   path: '', redirectTo: '/signup', pathMatch: 'full'
+  // }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { CarouselComponent } from './carousel/carousel.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
-import { LoginComponent } from './login/login.component';
-import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './user/login/login.component';
+import { SignupComponent } from './user/signup/signup.component';
 import { HomeComponent } from './home/home.component';
+import { WrapperComponent } from './Wrapper.component';
+import { ProductsComponent } from './products/products.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { CartComponent } from './cart/cart.component';
+import { WishlistComponent } from './wishlist/wishlist.component';
+import { UserComponent } from './user/user.component';
+import { User1Component } from './user/user1.component';
+// import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -18,12 +28,21 @@ import { HomeComponent } from './home/home.component';
     LoginComponent,
     SignupComponent,
     CarouselComponent,
-    HomeComponent
+    HomeComponent,
+    ProductsComponent,
+    NavigationComponent,
+    CartComponent,
+    WishlistComponent,
+    UserComponent,
+    User1Component,
+    WrapperComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
