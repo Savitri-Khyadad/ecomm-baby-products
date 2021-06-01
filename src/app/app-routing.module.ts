1c3164385da644d518260060dff36398dabf09c9
bergLowe <sumer2000singh@hotmail.com>
2021-06-01 20:24:05 +0530

    minor changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
-// import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path:'about',component:AboutComponent},
   {path:'contact',component:ContactComponent},
   {path:'login',component:LoginComponent},
