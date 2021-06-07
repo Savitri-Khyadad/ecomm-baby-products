6db5d88d07d6f49f2ac1fdcfaba18487c23a19d6
bergLowe <sumer2000singh@hotmail.com>
2021-06-08 02:23:06 +0530

    Integrated Login & Signup

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -4,57 +4,93 @@ import { UserService } from '../../shared/user.service';
 import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css'],
-  providers: [UserService]
+	selector: 'app-login',
+	templateUrl: './login.component.html',
+	styleUrls: ['./login.component.css'],
+	providers: [UserService]
 })
 export class LoginComponent implements OnInit {
 
-  fullName!: string;
-  email!: string;
-  password!:string;
+	// fullName!: string;
+	email!: string;
+	password!: string;
 
 
-  emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  showSucessMessage!: boolean;
-  serverErrorMessages!: string;
+	emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	showSucessMessage!: boolean;
+	serverErrorMessages!: string;
 
-  constructor(public userService: UserService,
-    private router:Router) { }
+	constructor(public userService: UserService,
+		private router: Router) { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+	}
+	
+	async onSubmit() {
 
-  onSubmit() {
-    this.userService.userLogin = true;
-    this.router.navigate(['products']);
-    console.log(this.userService.userLogin)
-    // this.userService.postUser(form.value).subscribe(
-    //   res => {
-    //     this.showSucessMessage = true;
-    //     setTimeout(() => this.showSucessMessage = false, 4000);
-    //     this.resetForm(form);
-    //   },
-    //   err => {
-    //     if (err.status === 422) {
-    //       this.serverErrorMessages = err.error.join('<br/>');
-    //     }
-    //     else
-    //       this.serverErrorMessages = 'Something went wrong.Please contact admin.';
-    //   }
-    // );
-  }
+		if (this.password.length < 7) {
+			this.serverErrorMessages = 'Password should be atleast 7 characters.';
+		}
 
-  // resetForm(form: NgForm) {
-  //   this.userService.selectedUser = {
-  //     name: '',
-  //     email: '',
-  //     password: ''
-  //   };
-  //   form.resetForm();
-  //   this.serverErrorMessages = '';
-  // }
+		if (!this.emailRegex.test(this.email) && this.email.length > 0) {
+			this.serverErrorMessages = 'Invalid Email Id.';
+		}
+
+		let content: any;
+		await fetch(`http://localhost:3000/users/login`, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ 
+                email: this.email, 
+                password: this.password 
+            })
+        }).then(async (res) => {
+            if (res.status === 200) {
+                content = await res.json();
+				localStorage.setItem("token", content.token);
+				// Those 3 lines already in the function before modification.
+				this.userService.userLogin = true;
+				console.log(this.userService.userLogin)
+				console.log(content)
+				this.router.navigate(['products']);
+            } else {
+				this.serverErrorMessages = 'Please check your credentials!'
+            }
+        }).catch(err => {
+			this.serverErrorMessages = 'Something went wrong!'
+			console.log(err)
+        });
+
+		/*---------------------------------------------------------------------*/
+
+		// this.userService.postUser(form.value).subscribe(
+		//   res => {
+		//     this.showSucessMessage = true;
+		//     setTimeout(() => this.showSucessMessage = false, 4000);
+		//     this.resetForm(form);
+		//   },
+		//   err => {
+		//     if (err.status === 422) {
+		//       this.serverErrorMessages = err.error.join('<br/>');
+		//     }
+		//     else
+		//       this.serverErrorMessages = 'Something went wrong.Please contact admin.';
+		//   }
+		// );
+	}
+
+	// resetForm(form: NgForm) {
+	//   this.userService.selectedUser = {
+	//     name: '',
+	//     email: '',
+	//     password: ''
+	//   };
+	//   form.resetForm();
+	//   this.serverErrorMessages = '';
+	// }
 
 
 }
--- src/app/user/signup/signup.component.ts
+++ src/app/user/signup/signup.component.ts
@@ -1,73 +1,105 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-signup',
-  templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css'],
+	selector: 'app-signup',
+	templateUrl: './signup.component.html',
+	styleUrls: ['./signup.component.css'],
 })
 export class SignupComponent implements OnInit {
 
-  fullName!: string;
-  email!: string;
-  password!:string;
+	fullName!: string;
+	email!: string;
+	password!: string;
 
 
-  emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  showSucessMessage!: boolean;
-  passwordError!: string;
-  serverErrorMessages!: string;
+	emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	showSucessMessage!: boolean;
+	passwordError!: string;
+	serverErrorMessages!: string;
 
-  constructor() {
+	constructor(private router: Router) {
 
-  }
+	}
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+	}
 
 
-  onSubmit(){
-    const user = {
-      name:this.fullName,
-      email:this.email,
-      password:this.password
-    }
-    if(this.password.length < 4){
-      this.serverErrorMessages = 'Password should be atleast 4 characters.';
-    }
+	async onSubmit() {
+		// const user = {
+		// 	name: this.fullName,
+		// 	email: this.email,
+		// 	password: this.password
+		// }
+		if (this.password.length < 7) {
+			this.serverErrorMessages = 'Password should be atleast 7 characters.';
+		}
 
-    if(!this.emailRegex.test(this.email) && this.email.length>0){
-      this.serverErrorMessages = 'Invalid Email Id.';
-    }
-    return false
-  }
+		if (!this.emailRegex.test(this.email) && this.email.length > 0) {
+			this.serverErrorMessages = 'Invalid Email Id.';
+		}
 
-  // onSubmit(form: NgForm) {
-  //   this.userService.postUser(form.value).subscribe(
-  //     res => {
-  //       this.showSucessMessage = true;
-  //       setTimeout(() => this.showSucessMessage = false, 4000);
-  //       this.resetForm(form);
-  //     },
-  //     err => {
-  //       if (err.status === 422) {
-  //         this.serverErrorMessages = err.error.join('<br/>');
-  //       }
-  //       else
-  //         this.serverErrorMessages = 'Something went wrong.Please contact admin.';
-  //     }
-  //   );
+		var content: any;
+		await(async () => {
+			await fetch(`http://localhost:3000/users`, {
+				method: 'POST',
+				headers: {
+					'Accept': 'application/json',
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify({
+					name: this.fullName,
+					email: this.email,
+					password: this.password
+				})
+			}).then(async (res) => {
+				// console.log(res.json())
+				if (res.status === 201) {
+					content = await res.json();
+					localStorage.setItem("token", content.token);
+					console.log(content)
+					// Redirected to different page.
+					this.router.navigate(['/products']);
+				} else {
+					this.serverErrorMessages = 'Email address may already exist or something else. Please Check Again!';
+				}
+			}).catch(err => {
+				this.serverErrorMessages = `There's some issue. Please Try Again!`;
+			});
+		})();
+		
+		// return false
+		return true
+	}	
 
-  // }
+	// onSubmit(form: NgForm) {
+	//   this.userService.postUser(form.value).subscribe(
+	//     res => {
+	//       this.showSucessMessage = true;
+	//       setTimeout(() => this.showSucessMessage = false, 4000);
+	//       this.resetForm(form);
+	//     },
+	//     err => {
+	//       if (err.status === 422) {
+	//         this.serverErrorMessages = err.error.join('<br/>');
+	//       }
+	//       else
+	//         this.serverErrorMessages = 'Something went wrong.Please contact admin.';
+	//     }
+	//   );
 
-  // resetForm(form: NgForm) {
-  //   this.userService.selectedUser = {
-  //     name: '',
-  //     email: '',
-  //     password: ''
-  //   };
-  //   form.resetForm();
-  //   this.serverErrorMessages = '';
-  // }
+	// }
+
+	// resetForm(form: NgForm) {
+	//   this.userService.selectedUser = {
+	//     name: '',
+	//     email: '',
+	//     password: ''
+	//   };
+	//   form.resetForm();
+	//   this.serverErrorMessages = '';
+	// }
 
 }
