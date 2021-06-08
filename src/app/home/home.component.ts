b1799dcb65c80651a55077761ec472175d2a1e20
bergLowe <sumer2000singh@hotmail.com>
2021-06-08 21:00:44 +0530

    Added Advice

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,10 +7,28 @@ import {CarouselComponent} from '../carousel/carousel.component';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  advice = {
+    advice: "",
+    id: '0'
+  };
   constructor() { }
 
   ngOnInit(): void {
+    this.getAdvice(this.advice.id);
+    setInterval(() => this.getAdvice(this.advice.id), 1210000000);
+  }
+
+  getAdvice = async (id: string = '0') => {
+    await fetch(`http://localhost:3000/ped/${id}`)
+    .then(async (res) => {
+      if (res.status === 200) {
+        const content = await res.json();
+        this.advice.advice = content.advice;
+        this.advice.id = content._id.toString();
+        console.log(this.advice)
+      }
+    })
+    .catch(err => console.log(err));
   }
 
 }
